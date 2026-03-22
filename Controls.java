ea07ae242e1a5a95427fb6d2b2fd7ec1d392dc41
Bebron28 <basurmanovstepan28@gmail.com>
2026-03-22 19:31:43 +0300

    Update Controls.java
    Обновил Controls под CooldownButton

--- Controls.java
+++ Controls.java
@@ -5,19 +5,32 @@ public class Controls extends JPanel {
     public Controls(Engine game) {
         setLayout(new FlowLayout());
 
-        add(new Button("Spawn Base", new GameObject() /* under construction */, () -> {
-            System.out.println("spawn Base");
-            // under construction
-        }));
+        CooldownButton spawnBaseBtn = new CooldownButton("Spawn Base", new GameObject() /* under construction */, () -> 
+                {
+                    System.out.println("spawn Base");
+                    // under construction
+                },
+                90
+        );
+        add(spawnBaseBtn);
 
-        add(new Button("Spawn Archer", new UnitArcher() , () -> {
-            // under construction
-        }));
+        CooldownButton spawnArcherBtn = new CooldownButton("Spawn Archer", new UnitArcher(), () -> 
+                {
+                    System.out.println("spawn Archer");
+                    // under construction
+                },
+                90
+        );
+        add(spawnArcherBtn);
 
-        add(new Button("Spawn Tank", new UnitDinoRider(), () -> {
-            System.out.println("spawn Tank");
-            // under construction
-            game.spawnObject(new Arrow(10, 100, -40, 400));
-        }));
+        CooldownButton spawnTankBtn = new CooldownButton("Spawn Tank", new UnitDinoRider(), () -> 
+                {
+                    System.out.println("spawn Tank");
+                    // under construction
+                    game.spawnObject(new Arrow(10, 100, -40, 400));
+                },
+                90
+        );
+        add(spawnTankBtn);
     }
 }
